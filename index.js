2b53bcf1467fd97af07364095d09bc17bd2d2519
Paris Dolfman <parisdolfman@gmail.com>
2018-11-27 19:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,9 @@ function sayHiToGrandma(string) {
  if (string === string.toLowerCase()) {
    return "I can't hear you!"
  }
+
+if (string === string.toUpperCase()) {
+  return "YES INDEED!"
+}
+ 
  }
